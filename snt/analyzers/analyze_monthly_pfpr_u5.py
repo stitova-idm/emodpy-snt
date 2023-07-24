2788d844fdbe22856e12bdcc316c514c5253d9aa
zdu <zdu@idmod.org>
2023-07-24 05:50:59 -0700

    copy original files

--- snt/analyzers/analyze_monthly_pfpr_u5.py
+++ snt/analyzers/analyze_monthly_pfpr_u5.py
@@ -1,65 +1,61 @@
-###########################
-# TODO: TO BE CONVERTED   #
-###########################
+import os
+import numpy as np
+import pandas as pd
+from simtools.Analysis.BaseAnalyzers import BaseAnalyzer
 
-# import os
-# import numpy as np
-# import pandas as pd
-# from simtools.Analysis.BaseAnalyzers import BaseAnalyzer
-#
-#
-# class MonthlyPfPRU5Analyzer(BaseAnalyzer):
-#
-#     def __init__(self, expt_name, sweep_variables=None, working_dir=".", start_year=2010, end_year=2016,
-#                  input_filename_base='MalariaSummaryReport_Monthly'):
-#         super(MonthlyPfPRU5Analyzer, self).__init__(working_dir=working_dir,
-#                                                     filenames=["output/%s%d.json" % (input_filename_base, x)
-#                                                                for x in range(start_year, (1+end_year))]
-#                                                     )
-#         self.sweep_variables = sweep_variables or ["Run_Number"]
-#         self.mult_param = 'Habitat_Multiplier'
-#         self.expt_name = expt_name
-#         self.start_year = start_year
-#         self.end_year = end_year
-#
-#
-#     def select_simulation_data(self, data, simulation):
-#
-#         adf = pd.DataFrame()
-#         aa = 1  # index for the 0.5-5 year age group
-#         for year, fname in zip(range(self.start_year, (self.end_year+1)), self.filenames):
-#             # population size
-#             pop = data[fname]['DataByTimeAndAgeBins']['Average Population by Age Bin'][:12]  # remove final five days: assume final five days have same average as rest of month
-#             pop_monthly = [x[aa] for x in pop]
-#             # PfPR
-#             d = data[fname]['DataByTimeAndAgeBins']['PfPR by Age Bin'][:12]  # remove final five days: assume final five days have same average as rest of month
-#             pfpr_monthly = [x[aa] for x in d]
-#             # combine in data frame
-#             simdata = pd.DataFrame({'month': list(range(1, 13)),
-#                                     'Pop': pop_monthly,
-#                                     'PfPR U5': pfpr_monthly,
-#                                     })
-#             simdata['year'] = year
-#             adf = pd.concat([adf, simdata])
-#
-#         for sweep_var in self.sweep_variables:
-#             if sweep_var in simulation.tags.keys():
-#                 adf[sweep_var] = simulation.tags[sweep_var]
-#         return adf
-#
-#     def finalize(self, all_data):
-#
-#         selected = [data for sim, data in all_data.items()]
-#         if len(selected) == 0:
-#             print("No data have been returned... Exiting...")
-#             return
-#
-#         if not os.path.exists(self.working_dir):
-#             os.mkdir(self.working_dir)
-#
-#         all_df = pd.concat(selected).reset_index(drop=True)
-#
-#         # don't take average across runs; calculate the best xLH for each seed (each of which uses different quantile)
-#         # all_df = all_df.groupby([self.mult_param, 'month', 'admin_name'])[['PfPR U5', 'Pop']].agg(np.mean).reset_index()
-#         # all_df = all_df.sort_values(by=[self.mult_param, 'month', 'archetype', 'DS_Name_for_ITN'])
-#         all_df.to_csv(os.path.join(self.working_dir, 'monthly_U5_PfPR.csv'), index=False)
+
+class MonthlyPfPRU5Analyzer(BaseAnalyzer):
+
+    def __init__(self, expt_name, sweep_variables=None, working_dir=".", start_year=2010, end_year=2016,
+                 input_filename_base='MalariaSummaryReport_Monthly'):
+        super(MonthlyPfPRU5Analyzer, self).__init__(working_dir=working_dir,
+                                                    filenames=["output/%s%d.json" % (input_filename_base, x)
+                                                               for x in range(start_year, (1+end_year))]
+                                                    )
+        self.sweep_variables = sweep_variables or ["Run_Number"]
+        self.mult_param = 'Habitat_Multiplier'
+        self.expt_name = expt_name
+        self.start_year = start_year
+        self.end_year = end_year
+
+
+    def select_simulation_data(self, data, simulation):
+
+        adf = pd.DataFrame()
+        aa = 1  # index for the 0.5-5 year age group
+        for year, fname in zip(range(self.start_year, (self.end_year+1)), self.filenames):
+            # population size
+            pop = data[fname]['DataByTimeAndAgeBins']['Average Population by Age Bin'][:12]  # remove final five days: assume final five days have same average as rest of month
+            pop_monthly = [x[aa] for x in pop]
+            # PfPR
+            d = data[fname]['DataByTimeAndAgeBins']['PfPR by Age Bin'][:12]  # remove final five days: assume final five days have same average as rest of month
+            pfpr_monthly = [x[aa] for x in d]
+            # combine in data frame
+            simdata = pd.DataFrame({'month': list(range(1, 13)),
+                                    'Pop': pop_monthly,
+                                    'PfPR U5': pfpr_monthly,
+                                    })
+            simdata['year'] = year
+            adf = pd.concat([adf, simdata])
+
+        for sweep_var in self.sweep_variables:
+            if sweep_var in simulation.tags.keys():
+                adf[sweep_var] = simulation.tags[sweep_var]
+        return adf
+
+    def finalize(self, all_data):
+
+        selected = [data for sim, data in all_data.items()]
+        if len(selected) == 0:
+            print("No data have been returned... Exiting...")
+            return
+
+        if not os.path.exists(self.working_dir):
+            os.mkdir(self.working_dir)
+
+        all_df = pd.concat(selected).reset_index(drop=True)
+
+        # don't take average across runs; calculate the best xLH for each seed (each of which uses different quantile)
+        # all_df = all_df.groupby([self.mult_param, 'month', 'admin_name'])[['PfPR U5', 'Pop']].agg(np.mean).reset_index()
+        # all_df = all_df.sort_values(by=[self.mult_param, 'month', 'archetype', 'DS_Name_for_ITN'])
+        all_df.to_csv(os.path.join(self.working_dir, 'monthly_U5_PfPR.csv'), index=False)
--- snt/analyzers/analyze_vector_numbers.py
+++ snt/analyzers/analyze_vector_numbers.py
@@ -1,69 +1,65 @@
-###########################
-# TODO: TO BE CONVERTED   #
-###########################
+import pandas as pd
+import numpy as np
+from simtools.Analysis.BaseAnalyzers import BaseAnalyzer
+import datetime
+import os
+import sys
+sys.path.append('../')
 
-# import pandas as pd
-# import numpy as np
-# from simtools.Analysis.BaseAnalyzers import BaseAnalyzer
-# import datetime
-# import os
-# import sys
-# sys.path.append('../')
-#
-#
-#
-# class VectorNumbersAnalyzer(BaseAnalyzer):
-#
-#     @classmethod
-#     def monthparser(self, x):
-#         if x == 0:
-#             return 12
-#         else:
-#             return datetime.datetime.strptime(str(x), '%j').month
-#
-#     def __init__(self, expt_name, sweep_variables=None, working_dir=".", start_year=2020, end_year=2026):
-#         super(VectorNumbersAnalyzer, self).__init__(working_dir=working_dir,
-#                                                     filenames=["output/ReportMalariaFiltered.json"])
-#         self.sweep_variables = sweep_variables or ["admin_name", "Run_Number"]
-#         self.inset_channels = ['Adult Vectors']
-#         self.expt_name = expt_name
-#         self.start_year = start_year
-#         self.end_year = end_year
-#
-#     # added to bypass failed cases
-#     # def filter(self, simulation):
-#     #     return simulation.status.name == 'Succeeded'
-#
-#     def select_simulation_data(self, data, simulation):
-#
-#         simdata = pd.DataFrame({x: data[self.filenames[0]]['Channels'][x]['Data'] for x in self.inset_channels})
-#         simdata['Time'] = simdata.index
-#
-#         simdata['Day'] = simdata['Time'] % 365
-#         simdata['month'] = simdata['Day'].apply(lambda x: self.monthparser((x + 1) % 365))
-#         simdata['year'] = simdata['Time'].apply(lambda x: int(x / 365) + self.start_year)
-#
-#         for sweep_var in self.sweep_variables:
-#             if sweep_var in simulation.tags.keys():
-#                 simdata[sweep_var] = simulation.tags[sweep_var]
-#         return simdata
-#
-#     def finalize(self, all_data):
-#
-#         selected = [data for sim, data in all_data.items()]
-#         if len(selected) == 0:
-#             print("No data have been returned... Exiting...")
-#             return
-#
-#         if not os.path.exists(os.path.join(self.working_dir, self.expt_name)):
-#             os.mkdir(os.path.join(self.working_dir, self.expt_name))
-#
-#         adf = pd.concat(selected).reset_index(drop=True)
-#         adf['date'] = adf.apply(lambda x: datetime.date(x['year'], x['month'], 1), axis=1)
-#
-#
-#         mean_channels = ['Adult Vectors']
-#
-#         adf = adf.groupby(['admin_name', 'date', 'Run_Number'])[mean_channels].agg(np.mean).reset_index()
-#         adf.to_csv(os.path.join(self.working_dir, self.expt_name, 'vector_numbers_monthly.csv'), index=False)
-#
+
+
+class VectorNumbersAnalyzer(BaseAnalyzer):
+
+    @classmethod
+    def monthparser(self, x):
+        if x == 0:
+            return 12
+        else:
+            return datetime.datetime.strptime(str(x), '%j').month
+
+    def __init__(self, expt_name, sweep_variables=None, working_dir=".", start_year=2020, end_year=2026):
+        super(VectorNumbersAnalyzer, self).__init__(working_dir=working_dir,
+                                                    filenames=["output/ReportMalariaFiltered.json"])
+        self.sweep_variables = sweep_variables or ["admin_name", "Run_Number"]
+        self.inset_channels = ['Adult Vectors']
+        self.expt_name = expt_name
+        self.start_year = start_year
+        self.end_year = end_year
+
+    # added to bypass failed cases
+    # def filter(self, simulation):
+    #     return simulation.status.name == 'Succeeded'
+
+    def select_simulation_data(self, data, simulation):
+
+        simdata = pd.DataFrame({x: data[self.filenames[0]]['Channels'][x]['Data'] for x in self.inset_channels})
+        simdata['Time'] = simdata.index
+
+        simdata['Day'] = simdata['Time'] % 365
+        simdata['month'] = simdata['Day'].apply(lambda x: self.monthparser((x + 1) % 365))
+        simdata['year'] = simdata['Time'].apply(lambda x: int(x / 365) + self.start_year)
+
+        for sweep_var in self.sweep_variables:
+            if sweep_var in simulation.tags.keys():
+                simdata[sweep_var] = simulation.tags[sweep_var]
+        return simdata
+
+    def finalize(self, all_data):
+
+        selected = [data for sim, data in all_data.items()]
+        if len(selected) == 0:
+            print("No data have been returned... Exiting...")
+            return
+
+        if not os.path.exists(os.path.join(self.working_dir, self.expt_name)):
+            os.mkdir(os.path.join(self.working_dir, self.expt_name))
+
+        adf = pd.concat(selected).reset_index(drop=True)
+        adf['date'] = adf.apply(lambda x: datetime.date(x['year'], x['month'], 1), axis=1)
+
+
+        mean_channels = ['Adult Vectors']
+
+        adf = adf.groupby(['admin_name', 'date', 'Run_Number'])[mean_channels].agg(np.mean).reset_index()
+        adf.to_csv(os.path.join(self.working_dir, self.expt_name, 'vector_numbers_monthly.csv'), index=False)
+

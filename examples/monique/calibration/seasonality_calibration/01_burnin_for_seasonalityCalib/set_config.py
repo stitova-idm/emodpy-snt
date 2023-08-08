d33c9d6c8f93b1f47d083549e8b10f2247729399
zdu <zdu@idmod.org>
2023-08-08 10:46:07 -0700

    code cleanup: removing writing_type/reading_type = None, as those are the defaults

--- examples/monique/calibration/seasonality_calibration/01_burnin_for_seasonalityCalib/set_config.py
+++ examples/monique/calibration/seasonality_calibration/01_burnin_for_seasonalityCalib/set_config.py
@@ -19,14 +19,11 @@ def set_config(config):
     if params.serialize:
         config.parameters.Simulation_Duration = params.years * 365 + 1
         config.parameters.Serialization_Time_Steps = [365 * (params.years - 5), 365 * params.years]
-        config.parameters.Serialized_Population_Reading_Type = 'NONE'
         config.parameters.Serialized_Population_Writing_Type = 'TIMESTEP'
         config.parameters.Serialization_Mask_Node_Write = 0
         config.parameters.Serialization_Precision = 'REDUCED'
     else:
-        config.parameters.Serialized_Population_Reading_Type = 'NONE'
         config.parameters.Simulation_Duration = params.years * 365 + 1
-        config.parameters.Serialized_Population_Writing_Type = 'NONE'
 
     # move to here from set_input_files
     config.parameters.Demographics_Filenames = [params.demographics_file]

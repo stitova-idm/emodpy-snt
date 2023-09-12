f7bdd5ad139bc4b8191336b867531fdee5f0a910
zdu <zdu@idmod.org>
2023-09-12 13:09:02 -0700

    keep original experiment id

--- examples/monique/calibration/seasonality_calibration/02_seasonality_calibration/params.py
+++ examples/monique/calibration/seasonality_calibration/02_seasonality_calibration/params.py
@@ -16,9 +16,7 @@ max_incidence = 100
 rep_admin = 'AA'
 # burn-in id should be listed for each of the archetypes
 burnin_ids = {
-    # 'AA': '7c9195e8-3d0a-ee11-aa07-b88303911bc1'
-    # 'AA': 'bd9177fc-ea35-ee11-aa09-b88303911bc1'    # Calculon
-    'AA': '7ff0bc4f-8943-ee11-aa09-b88303911bc1'
+    'AA': '7c9195e8-3d0a-ee11-aa07-b88303911bc1'
 }
 # set whether this is the first or second set of calibrations for this archetype
 round_number = 1  # <-- user needs to specify which round this is to pick up from prior round. Starts at round_number=1
